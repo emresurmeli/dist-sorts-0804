07daa7c9549684a7563193e3e8541c96fbffb116
emresurmeli <s.emre.s.8@gmail.com>
2025-09-03 11:32:38 -0400

    Answers provided

--- bucket-sort.js
+++ bucket-sort.js
@@ -7,13 +7,41 @@
 
 function bucketSort(array, bucketSize) {
   // Step 1: Determine the minimum and maximum values in the array
+  // Using Math
+  const min = Math.min(...array);
+  const max = Math.max(...array);
 
+  // Not using JS standard lib
+  let minNum = array[0];
+  let maxNum = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > maxNum) {
+      maxNum = array[i];
+    } else if (array[i] < minNum) {
+      minNum = array[i];
+    }
+  }
   // Step 2: Initialize buckets
   // Default bucket size can be adjusted
+  bucketSize = bucketSize || 5;
+  const bucketCount = Math.floor((max - min) / bucketSize) + 1;
+  const buckets = new Array(bucketCount).fill().map(() => []);
+  console.log(buckets);
 
   // Step 3: Distribute input array values into buckets
+  for (let i = 0; i < array.length; i++) {
+    const bucketIndex = Math.floor((array[i] - min) / bucketSize);
+    buckets[bucketIndex].push(array[i]);
+  }
+  console.log(buckets)
 
   // Step 4: Sort each bucket and place back into the array
+  const sorted = [];
+  for (let i = 0; i < buckets.length; i++) {
+    sorted.push(...insertionSort(buckets[i]));
+  }
+  console.log(sorted);
+  return sorted;
 }
 
 // Helper function: Insertion Sort
